85d73b34c3e37299e439cc52e84027e4f58ea565
VictorIzquier2 <victorizquier2@WorkStation.localdomain>
2020-11-30 19:58:06 +0100

    done

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div className='Card'>
+      <img src={props.image} alt={props.title}/>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </div>
+  )
+}
+export default Card;
